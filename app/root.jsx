b99962a13672bc825b7cb2cde550659870b72979
jignesh.dhandhukiya <jignesh.d@mail.biztechcs.com>
2026-01-27 16:13:50 +0530

    minor changes

--- app/root.jsx
+++ app/root.jsx
@@ -6,7 +6,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+export const loader = async () => {
+  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+};
+
 export default function App() {
+  const { apiKey } = useLoaderData();
   return (
     <html>
       <head>
@@ -17,7 +22,10 @@ export default function App() {
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+        <script
+          src="https://cdn.shopify.com/shopifycloud/app-bridge.js"
+          data-api-key={apiKey}
+        ></script>
         <Meta />
         <Links />
       </head>
